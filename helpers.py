5e88fe7de712f60067dcb6263041b57d55a062bb
wphilipp <william_philipp@web.de>
2022-04-26 09:59:08 +0200

    added downsampling function

--- helpers.py
+++ helpers.py
@@ -1,5 +1,6 @@
 import os
 import numpy as np
+from scipy.signal import decimate
 
 def load_data(filename, type):
     if type == "test":
@@ -8,3 +9,12 @@ def load_data(filename, type):
         path = "data_raw/training"
     full_path = os.path.join(path, filename)
     return np.load(full_path)
+
+def downsample(data, target_size):
+    downsample_factor = int(data.shape[1]/target_size)
+    downsampled_data = np.zeros((data.shape[0], target_size, data.shape[2]))
+    
+    for i in range(data.shape[0]):
+        for j in range(data.shape[2]):
+            downsampled_data[i,:,j] = decimate(data[i,:,j], downsample_factor)
+    return downsampled_data
